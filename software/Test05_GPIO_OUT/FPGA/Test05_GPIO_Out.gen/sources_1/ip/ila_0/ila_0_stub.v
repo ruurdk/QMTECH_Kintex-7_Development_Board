47747412e33e9c3e7f6beaa86234188fb6790707
Ruurd Keizer <ruurdk@hotmail.com>
2024-01-26 20:39:45 +0100

    C# test

--- software/Test05_GPIO_OUT/FPGA/Test05_GPIO_Out.gen/sources_1/ip/ila_0/ila_0_stub.v
+++ software/Test05_GPIO_OUT/FPGA/Test05_GPIO_Out.gen/sources_1/ip/ila_0/ila_0_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
-// Date        : Thu Jan 25 20:06:28 2024
+// Date        : Fri Jan 26 17:54:23 2024
 // Host        : Ruurd-Desktop running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
-//               c:/dev/QMTECH_Kintex-7_Development_Board/software/Test05_GPIO_OUT_Waveform/Test05_GPIO_Out/Test05_GPIO_Out.gen/sources_1/ip/ila_0/ila_0_stub.v
+//               c:/dev/QMTECH_Kintex-7_Development_Board/software/Test05_GPIO_OUT/FPGA/Test05_GPIO_Out.gen/sources_1/ip/ila_0/ila_0_stub.v
 // Design      : ila_0
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xc7k325tffg676-1
---
+++ software/Test_GPIO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+Console.WriteLine("Testing GPIO. Press Ctrl+C to end.");
+
+int pin = 18;
+using var controller = new GpioController();
+controller.OpenPin(pin, PinMode.Output);
+
+bool ledOn = true;
+while (true)
+{
+    controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
+    Thread.Sleep(1000);
+    ledOn = !ledOn;
+}
